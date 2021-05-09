b1aa74cae8f0e2cba73340da6cf7e093a436ffc3
huanghaoming <1585685035@qq.com>
2021-05-09 16:33:14 +0800

    通过cron组件，整合了子组件绑定值问题

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- <ComponentChild :inputValue="content"></ComponentChild> -->
+
+    <cron :dialogVisible="false" v-model="cronExpress"></cron>
+  </div>
+</template>
+
+<script>
+import ComponentChild from "./components/ComponentChild";
+
+import cron from "./components/vue-cron/cron";
+export default {
+  name: "App",
+  data() {
+    return {
+      content: "hello",
+      cronExpress: "0 0 2 1 * ? *",
+      dialogVisible: false,
+    };
+  },
+  components: {
+    ComponentChild,
+    cron,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ComponentChild.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <el-tag>标签一</el-tag>
+
+    <el-input v-model="inputValue" placeholder="请输入内容"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["inputValue"],
+};
+</script>
+
+<style>
+</style>
