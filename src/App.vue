1c332529416af1bd50b8a2b6bfb513a46aeb0376
huanghaoming <1585685035@qq.com>
2021-06-01 22:42:07 +0800

    添加dropzone

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
     <!-- <ComponentChild v-model="content"></ComponentChild> -->
+    <vue-dropzone id="dropzone" :options="dropzoneOptions" :useCustomSlot=true>
+      <div class="dropzone-custom-content">
+        <h3 class="dropzone-custom-title">Drag and drop to upload content!</h3>
+        <div class="subtitle">...or click to select a file from your computer</div>
+      </div>
+    </vue-dropzone>
 
-    <cron
-      :cronDisable="cronDisable"
-      v-model="cronExpress"
-      @cronChange="cronChange"
-    ></cron>
+    <cron :cronDisable="cronDisable" v-model="cronExpress" @cronChange="cronChange"></cron>
     <el-button type="primary" @click="submitForm">提交表单</el-button>
     <el-button type="primary" @click="ban">禁止使用</el-button>
   </div>
@@ -14,7 +16,8 @@
 
 <script>
 // import ComponentChild from "./components/ComponentChild";
-
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
 import cron from "./components/vue-cron/cron";
 export default {
   name: "App",
@@ -28,6 +31,11 @@ export default {
   },
   data() {
     return {
+      dropzoneOptions: {
+        url: "https://httpbin.org/post",
+        thumbnailWidth: 200,
+        addRemoveLinks: true,
+      },
       cronExpress: "",
       cronDisable: false,
     };
@@ -46,6 +54,7 @@ export default {
   },
   components: {
     // ComponentChild,
+    vueDropzone: vue2Dropzone,
     cron,
   },
 };
@@ -59,5 +68,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.dropzone-custom-title {
+  margin-top: 0;
+  color: #00b782;
+}
+
+.subtitle {
+  color: #314b5f;
+}
+.dropzone:hover {
+  background: white;
+}
+.dropzone {
+  border: 2px dashed #0087f7;
+  border-radius: 5px;
+  background: white;
 }
 </style>
---
+++ src/App2.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <vue-dropzone id="dropzone" :options="dropzoneOptions" :useCustomSlot=true>
+      <div style="width:100%;height:100%;position:relative;display: flex;justify-content: center;align-items: center;">
+        <img style="position:absolute;left:0px;bottom:0px;" src="./assets/Hadoop.svg">
+        <div class="dropzone-custom-content">
+          <h3 class="dropzone-custom-title">Drag and drop to upload content!</h3>
+          <div class="subtitle">...or click to select a file from your computer</div>
+        </div>
+      </div>
+
+    </vue-dropzone>
+  </div>
+</template>
+
+<script>
+// import ComponentChild from "./components/ComponentChild";
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
+export default {
+  name: "App",
+  mounted() {},
+  data() {
+    return {
+      dropzoneOptions: {
+        url: "https://httpbin.org/post",
+        thumbnailWidth: 200,
+        addRemoveLinks: true,
+      },
+    };
+  },
+  methods: {},
+  components: {
+    vueDropzone: vue2Dropzone,
+  },
+};
+</script>
+
+<style>
+.dropzone-custom-title {
+  margin-top: 0;
+  color: #00b782;
+}
+
+.subtitle {
+  color: #314b5f;
+}
+
+.dropzone {
+  border: 2px dashed #0087f7;
+  border-radius: 5px;
+  width: 800px;
+  height: 300px;
+  background: #f7f7f7;
+  padding: 0;
+}
+.vue-dropzone:hover {
+  background: #f1f1f1;
+}
+.dropzone .dz-message {
+  width: 800px;
+  height: 300px;
+  margin: 0;
+}
+</style>
