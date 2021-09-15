3dc706c8713b7e1e51ff530cae8a50dc75e32fcf
huanghaoming <1585685035@qq.com>
2021-09-15 21:54:51 +0800

    添加nprogress多实例

---
+++ src/components/Nprogress.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="a">
+    <div class="a1">
+
+    </div>
+    <br>
+
+    <div class="a2">
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+//vue中引用
+import NProgress from "multi-nprogress/nprogress";
+import "multi-nprogress/nprogress.css";
+import Vue from "vue";
+
+export default {
+  mounted() {
+    var instance = NProgress()
+
+    instance.configure({ parent: ".a1", startOnInit: true });
+    instance.set(0.4);
+    console.info(instance);
+
+    var instance2 = NProgress()
+    instance2.configure({ parent: ".a2", startOnInit: true });
+    instance2.set(0.4);
+    console.info(instance2);
+  },
+};
+</script>
+
+<style>
+.a1 {
+  width: 500px;
+  height: 200px;
+}
+.a2 {
+  width: 500px;
+  height: 200px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 // import App from './SqlPanel/Index.vue'
 // import App from './components/ztree/ZTree.vue'
 // import App from './components/rxjs/RxJsDemo'
-import App from './components/g6'
+// import App from './components/g6'
+import App from './components/Nprogress'
 
 require('./mock');
 
